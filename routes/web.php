ab299c0265dd91022288fe5b4eedfc9162624b96
roozbehsayadi <roozbehsayadi@gmail.com>
2019-04-18 15:16:50 +0430

    Nothing important, just using the "Route::view" method.

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::get('/', function () {
 Route::get('/welcome', function() {
    return 'Welcome to you';
 });
-
+/*
 Route::get('/test', function() {
 	return view( 'test' );
 });
+*/
+Route::view( '/test', 'test' );
